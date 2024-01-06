3e2c6edb39429a61aec18b881916186138cff765
Aman Kumar Rai <tej171995@gmail.com>
2024-01-06 19:15:54 +0530

    added new stuffs in redirection

--- middleware.js
+++ middleware.js
@@ -1,10 +1,21 @@
 // this meddleware is used in routed to verify wheteher user is logged in or not
 module.exports.isLoggedIn = (req, res, next) => {
-    // req.user automatically get stored with deserialsed info from the session
-    // console.log("REQ.USER.....", req.user)
-    if(!req.isAuthenticated()) {
-        req.flash('error', 'You must be signed in first');
-        return res.redirect('/login'); 
+    if (!req.isAuthenticated()) {
+        // undifined
+        console.log(req.session.returnTo)
+        req.session.returnTo = req.originalUrl; // add this line
+        console.log(req.session.returnTo)
+        req.flash('error', 'You must be signed in first!');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        console.log(res.locals.returnTo)
+        res.locals.returnTo = req.session.returnTo;
+        console.log(res.locals.returnTo)
     }
     next();
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -14,22 +15,37 @@ router.post('/register', catchAsync(async (req, res) => {
         const {email, username, password} = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Yelpcamp');
-        res.redirect('/campgrounds');
+        // when you register then you don't need to sign in again
+        // below will automatically do it
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash('success', 'Welcome to Yelpcamp');
+            res.redirect('/campgrounds');
+        })
+        
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('register')
     } 
 }))
 
+
+
 router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-    req.flash('success', 'Welcome Back');
-    res.redirect('/campgrounds');
-})
+router.post('/login',
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    // passport.authenticate logs the user in and clears req.session
+    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+    // Now we can use res.locals.returnTo to redirect the user after login
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+        res.redirect(redirectUrl);
+    });
 
 router.get('/logout', (req, res, next) => {
     req.logout(function (err) {
